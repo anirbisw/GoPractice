d5fd2fdeb6489952815fdb82e305cd2dc76afe77
Aniruddha Biswas <aniruddhabiswas@gmail.com>
2022-04-23 20:05:38 +0530

    Initial Commit

---
+++ gopl/chapter1/Ex1.9/main.go
@@ -0,0 +1,28 @@
+// Fetch prints the status code of fetch of each specified URL.
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		b, err := io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Println("Bytes Read", b)
+		fmt.Printf("Status code %d\n", resp.StatusCode)
+	}
+}
